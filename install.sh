e257386d52c69301e1f64bcc93cbbeaad3fccab6
李雨田 <demon@danwi.me>
2016-01-29 10:47:29 +0800

    optimize install script

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 cd src
 # make
 echo "正在编译驱动"
-make
+make 1>>/dev/null 
 echo "编译完成"
+echo "准备安装,非root用户可能需要输入密码"
 # install
-sudo make install
+sudo rm -rf /etc/Wireless
+sudo make install 1>>/dev/null
 echo "安装完成"
 # load
-sudo modprobe mt7601Usta
+sudo modprobe mt7601Usta 1>>/dev/null
 echo "驱动已经加载,Enjoy it!"
